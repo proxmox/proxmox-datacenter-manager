1cfcd5babf131594b5d36ed2fbb18396b0ee957d
Wolfgang Bumiller <w.bumiller@proxmox.com>
2023-03-09 15:33:49 +0100

    use userid types from proxmox-auth-api
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- api/pdm-api-types/src/lib.rs
+++ api/pdm-api-types/src/lib.rs
@@ -16,14 +16,14 @@ pub mod common_regex;
 mod acl;
 pub use acl::*;
 
-#[macro_use]
-mod userid;
-pub use userid::Authid;
-pub use userid::Userid;
-pub use userid::{Realm, RealmRef};
-pub use userid::{Tokenname, TokennameRef};
-pub use userid::{Username, UsernameRef};
-pub use userid::{PROXMOX_GROUP_ID_SCHEMA, PROXMOX_TOKEN_ID_SCHEMA, PROXMOX_TOKEN_NAME_SCHEMA};
+pub use proxmox_auth_api::types as userid;
+pub use proxmox_auth_api::types::{Authid, Userid};
+pub use proxmox_auth_api::types::{Realm, RealmRef};
+pub use proxmox_auth_api::types::{Tokenname, TokennameRef};
+pub use proxmox_auth_api::types::{Username, UsernameRef};
+pub use proxmox_auth_api::types::{
+    PROXMOX_GROUP_ID_SCHEMA, PROXMOX_TOKEN_ID_SCHEMA, PROXMOX_TOKEN_NAME_SCHEMA,
+};
 
 #[macro_use]
 mod user;
