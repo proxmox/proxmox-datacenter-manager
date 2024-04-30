3970f725718275883c4c056c96db69522e2bd6db
Dietmar Maurer <dietmar@proxmox.com>
2024-04-30 12:34:30 +0200

    api: dns: use strong rust types instead of json value.
    Signed-off-by: Dietmar Maurer <dietmar@proxmox.com>

--- api/server/src/api/nodes/dns.rs
+++ api/server/src/api/nodes/dns.rs
@@ -6,7 +6,7 @@ use const_format::concatcp;
 use lazy_static::lazy_static;
 use openssl::sha;
 use regex::Regex;
-use serde_json::{json, Value};
+use serde_json::Value;
 
 use pdm_api_types::IPRE_STR;
 
@@ -22,6 +22,59 @@ use pdm_api_types::{
 
 static RESOLV_CONF_FN: &str = "/etc/resolv.conf";
 
+#[api(
+    properties: {
+        search: {
+            schema: SEARCH_DOMAIN_SCHEMA,
+            optional: true,
+        },
+        dns1: {
+            optional: true,
+            schema: FIRST_DNS_SERVER_SCHEMA,
+        },
+        dns2: {
+            optional: true,
+            schema: SECOND_DNS_SERVER_SCHEMA,
+        },
+        dns3: {
+            optional: true,
+            schema: THIRD_DNS_SERVER_SCHEMA,
+        },
+        options: {
+            description: "Other data found in the configuration file (resolv.conf).",
+            optional: true,
+        },
+
+    }
+)]
+#[derive(Serialize, Deserialize, Default)]
+/// DNS configuration from '/etc/resolv.conf'
+pub struct ResolvConf {
+    pub search: Option<String>,
+    pub dns1: Option<String>,
+    pub dns2: Option<String>,
+    pub dns3: Option<String>,
+    pub options: Option<String>,
+}
+
+#[api(
+    properties: {
+        config: {
+            type: ResolvConf,
+        },
+        digest: {
+            schema: PROXMOX_CONFIG_DIGEST_SCHEMA,
+        },
+    }
+)]
+#[derive(Serialize, Deserialize)]
+/// DNS configuration with digest.
+pub struct ResolvConfWithDigest {
+    #[serde(flatten)]
+    pub config: ResolvConf,
+    pub digest: String,
+}
+
 #[api()]
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
@@ -35,8 +88,8 @@ pub enum DeletableProperty {
     Dns3,
 }
 
-pub fn read_etc_resolv_conf(expected_digest: Option<&str>) -> Result<Value, Error> {
-    let mut result = json!({});
+pub fn read_etc_resolv_conf(expected_digest: Option<&str>) -> Result<ResolvConfWithDigest, Error> {
+    let mut config = ResolvConf::default();
 
     let mut nscount = 0;
 
@@ -45,7 +98,7 @@ pub fn read_etc_resolv_conf(expected_digest: Option<&str>) -> Result<Value, Erro
 
     pdm_config::detect_modified_configuration_file(expected_digest, &digest)?;
 
-    result["digest"] = Value::from(hex::encode(digest));
+    let digest = hex::encode(digest);
 
     let data = String::from_utf8(raw)?;
 
@@ -59,15 +112,19 @@ pub fn read_etc_resolv_conf(expected_digest: Option<&str>) -> Result<Value, Erro
 
     for line in data.lines() {
         if let Some(caps) = DOMAIN_REGEX.captures(line) {
-            result["search"] = Value::from(&caps[1]);
+            config.search = Some(caps[1].to_owned());
         } else if let Some(caps) = SERVER_REGEX.captures(line) {
             nscount += 1;
             if nscount > 3 {
                 continue;
             };
-            let nameserver = &caps[1];
-            let id = format!("dns{}", nscount);
-            result[id] = Value::from(nameserver);
+            let nameserver = Some(caps[1].to_owned());
+            match nscount {
+                1 => config.dns1 = nameserver,
+                2 => config.dns2 = nameserver,
+                3 => config.dns3 = nameserver,
+                _ => continue,
+            }
         } else {
             if !options.is_empty() {
                 options.push('\n');
@@ -77,10 +134,10 @@ pub fn read_etc_resolv_conf(expected_digest: Option<&str>) -> Result<Value, Erro
     }
 
     if !options.is_empty() {
-        result["options"] = options.into();
+        config.options = Some(options);
     }
 
-    Ok(result)
+    Ok(ResolvConfWithDigest { config, digest })
 }
 
 #[api(
@@ -91,21 +148,9 @@ pub fn read_etc_resolv_conf(expected_digest: Option<&str>) -> Result<Value, Erro
             node: {
                 schema: NODE_SCHEMA,
             },
-            search: {
-                schema: SEARCH_DOMAIN_SCHEMA,
-                optional: true,
-            },
-            dns1: {
-                optional: true,
-                schema: FIRST_DNS_SERVER_SCHEMA,
-            },
-            dns2: {
-                optional: true,
-                schema: SECOND_DNS_SERVER_SCHEMA,
-            },
-            dns3: {
-                optional: true,
-                schema: THIRD_DNS_SERVER_SCHEMA,
+            update: {
+                type: ResolvConf,
+                flatten: true,
             },
             delete: {
                 description: "List of properties to delete.",
@@ -116,8 +161,8 @@ pub fn read_etc_resolv_conf(expected_digest: Option<&str>) -> Result<Value, Erro
                 }
             },
             digest: {
-                optional: true,
                 schema: PROXMOX_CONFIG_DIGEST_SCHEMA,
+                optional: true,
             },
         },
     },
@@ -127,69 +172,73 @@ pub fn read_etc_resolv_conf(expected_digest: Option<&str>) -> Result<Value, Erro
 )]
 /// Update DNS settings
 pub fn update_dns(
-    search: Option<String>,
-    dns1: Option<String>,
-    dns2: Option<String>,
-    dns3: Option<String>,
+    update: ResolvConf,
     delete: Option<Vec<DeletableProperty>>,
     digest: Option<String>,
-) -> Result<Value, Error> {
+) -> Result<(), Error> {
     lazy_static! {
         static ref MUTEX: Arc<Mutex<()>> = Arc::new(Mutex::new(()));
     }
 
     let _guard = MUTEX.lock();
 
-    let mut config = read_etc_resolv_conf(digest.as_deref())?;
+    let ResolvConfWithDigest { mut config, .. } = read_etc_resolv_conf(digest.as_deref())?;
 
     if let Some(delete) = delete {
         for delete_prop in delete {
-            let config = config.as_object_mut().unwrap();
             match delete_prop {
                 DeletableProperty::Dns1 => {
-                    config.remove("dns1");
+                    config.dns1 = None;
                 }
                 DeletableProperty::Dns2 => {
-                    config.remove("dns2");
+                    config.dns2 = None;
                 }
                 DeletableProperty::Dns3 => {
-                    config.remove("dns3");
+                    config.dns3 = None;
                 }
             }
         }
     }
 
-    if let Some(search) = search {
-        config["search"] = search.into();
+    if update.search.is_some() {
+        config.search = update.search;
     }
-    if let Some(dns1) = dns1 {
-        config["dns1"] = dns1.into();
+    if update.dns1.is_some() {
+        config.dns1 = update.dns1;
     }
-    if let Some(dns2) = dns2 {
-        config["dns2"] = dns2.into();
+    if update.dns2.is_some() {
+        config.dns2 = update.dns2;
     }
-    if let Some(dns3) = dns3 {
-        config["dns3"] = dns3.into();
+    if update.dns3.is_some() {
+        config.dns3 = update.dns3;
     }
 
     let mut data = String::new();
 
     use std::fmt::Write as _;
-    if let Some(search) = config["search"].as_str() {
+    if let Some(search) = config.search {
         let _ = writeln!(data, "search {}", search);
     }
-    for opt in &["dns1", "dns2", "dns3"] {
-        if let Some(server) = config[opt].as_str() {
-            let _ = writeln!(data, "nameserver {}", server);
-        }
+
+    if let Some(dns1) = config.dns1 {
+        let _ = writeln!(data, "nameserver {}", dns1);
     }
-    if let Some(options) = config["options"].as_str() {
-        data.push_str(options);
+
+    if let Some(dns2) = config.dns2 {
+        let _ = writeln!(data, "nameserver {}", dns2);
+    }
+
+    if let Some(dns3) = config.dns3 {
+        let _ = writeln!(data, "nameserver {}", dns3);
+    }
+
+    if let Some(options) = config.options {
+        data.push_str(&options);
     }
 
     replace_file(RESOLV_CONF_FN, data.as_bytes(), CreateOptions::new(), true)?;
 
-    Ok(Value::Null)
+    Ok(())
 }
 
 #[api(
@@ -201,29 +250,7 @@ pub fn update_dns(
         },
     },
     returns: {
-        description: "Returns DNS server IPs and sreach domain.",
-        type: Object,
-        properties: {
-            digest: {
-                schema: PROXMOX_CONFIG_DIGEST_SCHEMA,
-            },
-            search: {
-                optional: true,
-                schema: SEARCH_DOMAIN_SCHEMA,
-            },
-            dns1: {
-                optional: true,
-                schema: FIRST_DNS_SERVER_SCHEMA,
-            },
-            dns2: {
-                optional: true,
-                schema: SECOND_DNS_SERVER_SCHEMA,
-            },
-            dns3: {
-                optional: true,
-                schema: THIRD_DNS_SERVER_SCHEMA,
-            },
-        },
+        type: ResolvConfWithDigest,
     },
     access: {
         permission: &Permission::Privilege(&["system", "network", "dns"], PRIV_SYS_AUDIT, false),
@@ -234,7 +261,7 @@ pub fn get_dns(
     _param: Value,
     _info: &ApiMethod,
     _rpcenv: &mut dyn RpcEnvironment,
-) -> Result<Value, Error> {
+) -> Result<ResolvConfWithDigest, Error> {
     read_etc_resolv_conf(None)
 }
 
