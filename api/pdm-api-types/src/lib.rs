5185053eb3d3aba75a8e964b1c491e0a7637aca5
Dietmar Maurer <dietmar@proxmox.com>
2024-05-07 14:26:54 +0200

    use new proxmox-system-config-api crate

--- api/pdm-api-types/src/lib.rs
+++ api/pdm-api-types/src/lib.rs
@@ -62,10 +62,10 @@ pub use proxmox_schema::api_types::{PASSWORD_FORMAT, PASSWORD_SCHEMA};
 pub use proxmox_schema::api_types::{SERVICE_ID_SCHEMA, UUID_FORMAT};
 pub use proxmox_schema::api_types::{SYSTEMD_DATETIME_FORMAT, TIME_ZONE_SCHEMA};
 
-pub use proxmox_dns_api::SEARCH_DOMAIN_SCHEMA;
-pub use proxmox_dns_api::FIRST_DNS_SERVER_SCHEMA;
-pub use proxmox_dns_api::SECOND_DNS_SERVER_SCHEMA;
-pub use proxmox_dns_api::THIRD_DNS_SERVER_SCHEMA;
+pub use proxmox_system_config_api::SEARCH_DOMAIN_SCHEMA;
+pub use proxmox_system_config_api::FIRST_DNS_SERVER_SCHEMA;
+pub use proxmox_system_config_api::SECOND_DNS_SERVER_SCHEMA;
+pub use proxmox_system_config_api::THIRD_DNS_SERVER_SCHEMA;
 
 pub use proxmox_product_config::PROXMOX_CONFIG_DIGEST_SCHEMA;
 
--- api/server/src/api/nodes/time.rs
+++ api/server/src/api/nodes/time.rs
@@ -1,28 +1,11 @@
-use anyhow::{bail, format_err, Error};
-use serde_json::{json, Value};
+use anyhow::Error;
+use serde_json::Value;
 
 use proxmox_router::{Permission, Router};
 use proxmox_schema::api;
-use proxmox_sys::fs::{file_read_firstline, replace_file, CreateOptions};
 
 use pdm_api_types::{NODE_SCHEMA, PRIV_SYS_MODIFY, TIME_ZONE_SCHEMA};
-
-fn read_etc_localtime() -> Result<String, Error> {
-    // use /etc/timezone
-    if let Ok(line) = file_read_firstline("/etc/timezone") {
-        return Ok(line.trim().to_owned());
-    }
-
-    // otherwise guess from the /etc/localtime symlink
-    let link = std::fs::read_link("/etc/localtime")
-        .map_err(|err| format_err!("failed to guess timezone - {}", err))?;
-
-    let link = link.to_string_lossy();
-    match link.rfind("/zoneinfo/") {
-        Some(pos) => Ok(link[(pos + 10)..].to_string()),
-        None => Ok(link.to_string()),
-    }
-}
+use proxmox_system_config_api::ServerTimeInfo;
 
 #[api(
     input: {
@@ -33,40 +16,15 @@ fn read_etc_localtime() -> Result<String, Error> {
         },
     },
     returns: {
-        description: "Returns server time and timezone.",
-        properties: {
-            timezone: {
-                schema: TIME_ZONE_SCHEMA,
-            },
-            time: {
-                type: i64,
-                description: "Seconds since 1970-01-01 00:00:00 UTC.",
-                minimum: 1_297_163_644,
-            },
-            localtime: {
-                type: i64,
-                description: "Seconds since 1970-01-01 00:00:00 UTC. (local time)",
-                minimum: 1_297_163_644,
-            },
-        }
+        type: ServerTimeInfo,
     },
     access: {
         permission: &Permission::Anybody,
     },
 )]
 /// Read server time and time zone settings.
-fn get_time(_param: Value) -> Result<Value, Error> {
-    let time = proxmox_time::epoch_i64();
-    let tm = proxmox_time::localtime(time)?;
-    let offset = tm.tm_gmtoff;
-
-    let localtime = time + offset;
-
-    Ok(json!({
-        "timezone": read_etc_localtime()?,
-        "time": time,
-        "localtime": localtime,
-    }))
+fn get_time(_param: Value) -> Result<ServerTimeInfo, Error> {
+    proxmox_system_config_api::time::get_server_time_info()
 }
 
 #[api(
@@ -87,26 +45,8 @@ fn get_time(_param: Value) -> Result<Value, Error> {
     },
 )]
 /// Set time zone
-fn set_timezone(timezone: String, _param: Value) -> Result<Value, Error> {
-    let path = std::path::PathBuf::from(format!("/usr/share/zoneinfo/{}", timezone));
-
-    if !path.exists() {
-        bail!("No such timezone.");
-    }
-
-    replace_file(
-        "/etc/timezone",
-        timezone.as_bytes(),
-        CreateOptions::new(),
-        true,
-    )?;
-
-    let _ = std::fs::remove_file("/etc/localtime");
-
-    use std::os::unix::fs::symlink;
-    symlink(path, "/etc/localtime")?;
-
-    Ok(Value::Null)
+fn set_timezone(timezone: String, _param: Value) -> Result<(), Error> {
+    proxmox_system_config_api::time::set_timezone(timezone)
 }
 
 pub const ROUTER: Router = Router::new()
