f425328893c0af127d11dab00551baa48cca6f79
Dietmar Maurer <dietmar@proxmox.com>
2024-05-31 12:27:09 +0200

    make it compile again with latest product-config
    Signed-off-by: Dietmar Maurer <dietmar@proxmox.com>

--- api/server/src/bin/proxmox-datacenter-privileged-api.rs
+++ api/server/src/bin/proxmox-datacenter-privileged-api.rs
@@ -17,7 +17,7 @@ fn main() -> Result<(), Error> {
 
     server::env::sanitize_environment_vars();
 
-    proxmox_product_config::init_product_config(pdm_buildcfg::CONFIGDIR, pdm_config::api_user()?);
+    proxmox_product_config::init(pdm_config::api_user()?);
 
     let debug = std::env::var("PROXMOX_DEBUG").is_ok();
 
