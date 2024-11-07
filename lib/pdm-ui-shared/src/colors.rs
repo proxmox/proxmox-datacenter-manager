4d52cadecfc2724668133f0f2fcf5d96738b7a2d
Dominik Csapak <d.csapak@proxmox.com>
2024-11-07 11:24:22 +0100

    pdm-ui-shared: colors: add calculation for contrast and css rgb
    we need that for the ui and fits best here with the other color code
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- lib/pdm-ui-shared/src/colors.rs
+++ lib/pdm-ui-shared/src/colors.rs
@@ -45,6 +45,75 @@ pub fn text_to_rgb(text: &str) -> Option<Rgb> {
     })
 }
 
+/// Returns the best contrast color for the given RGB color value
+///
+/// returns either white rgb(255,255,255) or black rgb(0,0,0)
+// Taken from proxmox-widget-toolkit's src/Utils.js:
+//
+// optimized & simplified SAPC function
+// https://github.com/Myndex/SAPC-APCA
+// getTextContrastClass: function(rgb) {
+//     const blkThrs = 0.022;
+//     const blkClmp = 1.414;
+//
+//     // linearize & gamma correction
+//     let r = (rgb[0] / 255) ** 2.4;
+//     let g = (rgb[1] / 255) ** 2.4;
+//     let b = (rgb[2] / 255) ** 2.4;
+//
+//     // relative luminance sRGB
+//     let bg = r * 0.2126729 + g * 0.7151522 + b * 0.0721750;
+//
+//     // black clamp
+//     bg = bg > blkThrs ? bg : bg + (blkThrs - bg) ** blkClmp;
+//
+//     // SAPC with white text
+//     let contrastLight = bg ** 0.65 - 1;
+//     // SAPC with black text
+//     let contrastDark = bg ** 0.56 - 0.046134502;
+//
+//     if (Math.abs(contrastLight) >= Math.abs(contrastDark)) {
+//          'light';
+//     } else {
+//          'dark';
+//     }
+// },
+pub fn get_best_contrast_color(color: &Rgb) -> Rgb {
+    const BLACK_THRESHOLD: f64 = 0.022;
+    const BLACK_CLAMP: f64 = 1.414;
+
+    // linearize & gamma correct
+    let r = (color.r as f64 / 255.0).powf(2.4);
+    let g = (color.g as f64 / 255.0).powf(2.4);
+    let b = (color.b as f64 / 255.0).powf(2.4);
+
+    // relative luminance sRGB
+    let bg = r * 0.2126729 + g * 0.7151522 + b * 0.0721750;
+
+    // black clamp
+    let bg = if bg > BLACK_THRESHOLD {
+        bg
+    } else {
+        bg + (BLACK_THRESHOLD - bg).powf(BLACK_CLAMP)
+    };
+
+    // SAPC with white text
+    let light = bg.powf(0.65) - 1.0;
+
+    // SAPC with black text
+    let dark = bg.powf(0.56) - 0.046134502;
+
+    if light.abs() >= dark.abs() {
+        Rgb {
+            r: 255,
+            g: 255,
+            b: 255,
+        }
+    } else {
+        Rgb { r: 0, g: 0, b: 0 }
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 pub struct TtyResetColor;
 
@@ -64,6 +133,20 @@ pub struct Rgb {
 impl Rgb {
     pub fn as_ansi(&self) -> AnsiRgb<'_> {
         AnsiRgb(self)
+    }
+
+    pub fn as_css_rgb(&self) -> CssRgb<'_> {
+        CssRgb(self)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct CssRgb<'a>(&'a Rgb);
+
+impl fmt::Display for CssRgb<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let Rgb { r, g, b } = *self.0;
+        write!(f, "rgb({r}, {g}, {b})")
     }
 }
 
