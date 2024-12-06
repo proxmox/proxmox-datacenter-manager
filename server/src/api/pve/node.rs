038d2e568c02d5915774bc9829ea25a16ecc7374
Dominik Csapak <d.csapak@proxmox.com>
2024-12-06 14:45:03 +0100

    server: api: pve: add get network api call
    gets the network interface list from pve
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- server/src/api/pve/node.rs
+++ server/src/api/pve/node.rs
@@ -11,4 +11,31 @@ pub const ROUTER: Router = Router::new()
     .subdirs(SUBDIRS);
 
 #[sortable]
-const SUBDIRS: SubdirMap = &sorted!([("rrddata", &super::rrddata::NODE_RRD_ROUTER),]);
+const SUBDIRS: SubdirMap = &sorted!([
+    ("rrddata", &super::rrddata::NODE_RRD_ROUTER),
+    ("network", &Router::new().get(&API_METHOD_GET_NETWORK))
+]);
+
+#[api(
+    input: {
+        properties: {
+            remote: { schema: REMOTE_ID_SCHEMA },
+            node: { schema: NODE_SCHEMA },
+            "interface-type": {
+                type: pve_api_types::ListNetworksType,
+                optional: true,
+            },
+        },
+    },
+)]
+/// Get network interfaces from PVE node
+async fn get_network(
+    remote: String,
+    node: String,
+    interface_type: Option<pve_api_types::ListNetworksType>,
+) -> Result<Vec<pve_api_types::NetworkInterface>, Error> {
+    let (remotes, _) = pdm_config::remotes::config()?;
+    let client = super::connect_to_remote(&remotes, &remote)?;
+    let networks = client.list_networks(&node, interface_type).await?;
+    Ok(networks)
+}
