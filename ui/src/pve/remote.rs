2ca824dcb852f590e8a3de8d2f1e3c8f58e85c75
Dominik Csapak <d.csapak@proxmox.com>
2024-12-13 15:55:31 +0100

    ui: pve: remote panel: show useful information
    the Subscription status, how many nodes exist, how many guests are
    running (and exist), assigned cpu cores, memory, cpu/memory usage (avg
    over hosts), total storage usage
    
    how we display the info here is not final, e.g. the assigned cores info
    is useful, but currently is a bit hard to understand what is meant
    exactly. also the storage accumulation should probably follow how we do
    things in pve itself, but those things can always be fixed later too.
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- ui/src/pve/remote.rs
+++ ui/src/pve/remote.rs
@@ -1,16 +1,20 @@
 use std::rc::Rc;
 
+use gloo_utils::window;
 use yew::Properties;
 
+use proxmox_human_byte::HumanByte;
 use pwt::{
     css::AlignItems,
     prelude::*,
     props::WidgetBuilder,
-    widget::{Fa, Panel, Row},
+    widget::{error_message, ActionIcon, Column, Fa, Meter, Panel, Row},
 };
 use pwt_macros::widget;
 
 use pdm_api_types::resource::PveResource;
+
+use crate::get_deep_url;
 
 #[widget(comp=RemotePanelComp, @element)]
 #[derive(Clone, Debug, PartialEq, Properties)]
@@ -30,7 +34,116 @@ impl RemotePanel {
     }
 }
 
-pub struct RemotePanelComp {}
+pub struct RemotePanelComp {
+    status: RemoteStatus,
+}
+
+#[derive(Default)]
+struct RemoteStatus {
+    guests: i32,
+    guests_running: i32,
+    guest_cores_running: f64,
+    guest_cores: f64,
+    guest_memory_running: u64,
+    guest_memory: u64,
+    max_cores: f64,
+    memory_usage: f64,
+    max_memory: u64,
+    storage: u64,
+    max_storage: u64,
+    nodes: i32,
+    cpu_usage: f64,
+    level: &'static str,
+}
+
+impl RemotePanelComp {
+    fn calculate_status(&mut self, ctx: &yew::Context<Self>) {
+        let mut guests = 0;
+        let mut guests_running = 0;
+        let mut guest_cores_running = 0.0;
+        let mut guest_cores = 0.0;
+        let mut guest_memory_running = 0;
+        let mut guest_memory = 0;
+        let mut max_cores = 0.0;
+        let mut memory_usage = 0.0;
+        let mut max_memory = 0;
+        let mut storage = 0;
+        let mut max_storage = 1;
+        let mut nodes = 0;
+        let mut cpu_usage = 0.0;
+        let mut level = None;
+
+        for res in ctx.props().resources.iter() {
+            match res {
+                PveResource::Storage(store) => {
+                    storage += store.disk;
+                    max_storage += store.maxdisk;
+                }
+                PveResource::Qemu(qemu) => {
+                    guests += 1;
+                    if qemu.status == "stopped" {
+                        guest_cores += qemu.maxcpu;
+                        guest_memory += qemu.maxmem;
+                    } else {
+                        guests_running += 1;
+                        guest_cores_running += qemu.maxcpu;
+                        guest_memory_running += qemu.maxmem;
+                    }
+                }
+                PveResource::Lxc(lxc) => {
+                    guests += 1;
+                    if lxc.status == "stopped" {
+                        guest_cores += lxc.maxcpu;
+                        guest_memory += lxc.maxmem;
+                    } else {
+                        guest_cores_running += lxc.maxcpu;
+                        guest_memory_running += lxc.maxmem;
+                    }
+                }
+                PveResource::Node(node) => {
+                    nodes += 1;
+                    max_cores += node.maxcpu;
+                    max_memory += node.maxmem;
+                    cpu_usage += node.cpu;
+
+                    memory_usage += node.mem as f64 / node.maxmem as f64;
+
+                    match (node.level.as_str(), level) {
+                        (x, Some(y)) if x == y => {}
+                        (x, Some(y)) if x != y => {
+                            level = Some("");
+                        }
+                        ("c", None) => level = Some("Community"),
+                        ("b", None) => level = Some("Basic"),
+                        ("s", None) => level = Some("Standard"),
+                        ("p", None) => level = Some("Premium"),
+                        _ => level = Some(""),
+                    }
+                }
+            }
+        }
+
+        let cpu_usage = cpu_usage / nodes as f64;
+        let memory_usage = memory_usage / nodes as f64;
+
+        self.status = RemoteStatus {
+            guests,
+            guests_running,
+            guest_cores_running,
+            guest_cores,
+            guest_memory_running,
+            guest_memory,
+            max_cores,
+            memory_usage,
+            max_memory,
+            storage,
+            max_storage,
+            nodes,
+            cpu_usage,
+            level: level.unwrap_or_default(),
+        };
+    }
+}
 
 impl yew::Component for RemotePanelComp {
     type Message = ();
@@ -38,7 +151,16 @@ impl yew::Component for RemotePanelComp {
     type Properties = RemotePanel;
 
     fn create(_ctx: &yew::Context<Self>) -> Self {
-        Self {}
+        Self {
+            status: Default::default(),
+        }
+    }
+
+    fn changed(&mut self, ctx: &Context<Self>, old_props: &Self::Properties) -> bool {
+        if ctx.props().resources != old_props.resources {
+            self.calculate_status(ctx);
+        }
+        true
     }
 
     fn view(&self, ctx: &yew::Context<Self>) -> yew::Html {
@@ -48,9 +170,109 @@ impl yew::Component for RemotePanelComp {
             .with_child(Fa::new("server"))
             .with_child(tr! {"Remote '{0}'", ctx.props().remote})
             .into();
+
+        let status = &self.status;
+        let content = match &ctx.props().error {
+            Some(err) => Column::new().padding(4).with_child(error_message(err)),
+            None => Column::new()
+                .padding(4)
+                .gap(4)
+                .with_child(make_row(
+                    tr!("Subscription Status"),
+                    if status.level.is_empty() {
+                        "times-circle"
+                    } else {
+                        "check"
+                    },
+                    status.level.to_string(),
+                    None,
+                ))
+                .with_child(make_row(
+                    tr! {"Nodes"},
+                    "building",
+                    format!("{}", status.nodes),
+                    None,
+                ))
+                .with_child(make_row(
+                    tr! {"Guests running"},
+                    "desktop",
+                    tr!("{0} ({1} total)", status.guests_running, status.guests),
+                    None,
+                ))
+                .with_child(make_row(
+                    tr! {"Host Cores assigned"},
+                    "database",
+                    tr!(
+                        "{0} / {1} ({2} total configured)",
+                        status.guest_cores_running,
+                        status.max_cores,
+                        status.guest_cores,
+                    ),
+                    None,
+                ))
+                .with_child(make_row(
+                    tr! {"Host CPU usage (avg.)"},
+                    "database",
+                    format!("{:.2}%", status.cpu_usage * 100.0),
+                    Some(status.cpu_usage as f32),
+                ))
+                .with_child(make_row(
+                    tr! {"Host Memory assigned"},
+                    "list",
+                    tr!(
+                        "{0} / {1} ({2} total)",
+                        HumanByte::from(status.guest_memory_running),
+                        HumanByte::from(status.max_memory),
+                        HumanByte::from(status.guest_memory),
+                    ),
+                    Some((status.guest_memory_running as f64 / status.max_memory as f64) as f32),
+                ))
+                .with_child(make_row(
+                    tr! {"Host Memory usage (avg.)"},
+                    "list",
+                    format!("{:.2}%", status.memory_usage * 100.0),
+                    Some(status.memory_usage as f32),
+                ))
+                .with_child(make_row(
+                    tr! {"Host Storage used"},
+                    "server",
+                    format!(
+                        "{} / {}",
+                        HumanByte::from(status.storage),
+                        HumanByte::from(status.max_storage)
+                    ),
+                    Some((status.storage as f64 / status.max_storage as f64) as f32),
+                )),
+        };
+
         Panel::new()
             .with_std_props(&ctx.props().std_props)
             .title(title)
+            .with_tool(ActionIcon::new("fa fa-external-link").on_activate({
+                let link = ctx.link().clone();
+                let remote = ctx.props().remote.clone();
+                move |_| {
+                    if let Some(url) = get_deep_url(&link, &remote, "") {
+                        let _ = window().open_with_url(&url.href());
+                    }
+                }
+            }))
+            .with_child(content)
             .into()
     }
+}
+
+fn make_row(title: String, icon: &str, text: String, meter_value: Option<f32>) -> Column {
+    let row = Row::new()
+        .class(AlignItems::Baseline)
+        //.class(FontStyle::HeadlineSmall)
+        .gap(2)
+        .with_child(title)
+        .with_flex_spacer()
+        .with_child(text)
+        .with_child(Fa::new(icon).fixed_width());
+
+    Column::new().gap(1).with_child(row).with_optional_child(
+        meter_value.map(|value| Meter::new().optimum(0.0).low(0.7).high(0.9).value(value)),
+    )
 }
