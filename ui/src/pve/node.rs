0851bc22e11ece4e78f7f866c759ed66b22077ff
Dominik Csapak <d.csapak@proxmox.com>
2024-12-16 17:23:32 +0100

    ui: pve: node: add status header and additional rrd graph
    that shows some very useful information, similar to what we show in
    PVE. Also add the server load as additional RRDGraph.
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- ui/src/pve/node.rs
+++ ui/src/pve/node.rs
@@ -5,17 +5,21 @@ use yew::{
     Context,
 };
 
+use proxmox_human_byte::HumanByte;
+use proxmox_rrd_api_types::{RrdMode, RrdTimeframe};
 use proxmox_yew_comp::{RRDGraph, Series};
 use pwt::{
-    css::{ColorScheme, FlexFit},
+    css::{AlignItems, ColorScheme, Flex, FlexFit, Overflow},
     prelude::*,
     props::{ContainerBuilder, WidgetBuilder},
-    widget::{Column, Container, Panel},
+    widget::{error_message, Column, Container, Fa, Panel, Progress, Row},
     AsyncPool,
 };
 
 use pdm_api_types::rrddata::NodeDataPoint;
-use proxmox_rrd_api_types::{RrdMode, RrdTimeframe};
+use pdm_client::types::NodeStatus;
+
+use crate::renderer::separator;
 
 #[derive(Clone, Debug, Eq, PartialEq, Properties)]
 pub struct NodePanel {
@@ -26,8 +30,12 @@ pub struct NodePanel {
     pub node: String,
 
     #[prop_or(60_000)]
-    /// The interval for refreshing the data
-    pub interval: u32,
+    /// The interval for refreshing the rrd data
+    pub rrd_interval: u32,
+
+    #[prop_or(10_000)]
+    /// The interval for refreshing the status data
+    pub status_interval: u32,
 }
 
 impl NodePanel {
@@ -44,17 +52,25 @@ impl Into<VNode> for NodePanel {
 
 pub enum Msg {
     Reload,
+    ReloadStatus,
     LoadFinished(Result<Vec<NodeDataPoint>, proxmox_client::Error>),
+    StatusLoadFinished(Result<NodeStatus, proxmox_client::Error>),
 }
 
 pub struct NodePanelComp {
     time_data: Rc<Vec<i64>>,
     cpu_data: Rc<Series>,
+    load_data: Rc<Series>,
     mem_data: Rc<Series>,
     mem_total_data: Rc<Series>,
+    status: Option<NodeStatus>,
+
     last_error: Option<proxmox_client::Error>,
+    last_status_error: Option<proxmox_client::Error>,
+
     async_pool: AsyncPool,
     _timeout: Option<gloo_timers::callback::Timeout>,
+    _status_timeout: Option<gloo_timers::callback::Timeout>,
 }
 
 impl NodePanelComp {
@@ -65,6 +81,11 @@ impl NodePanelComp {
 
         Msg::LoadFinished(res)
     }
+
+    async fn reload_status(remote: &str, node: &str) -> Result<NodeStatus, proxmox_client::Error> {
+        let status = crate::pdm_client().pve_node_status(remote, node).await?;
+        Ok(status)
+    }
 }
 
 impl yew::Component for NodePanelComp {
@@ -73,14 +94,19 @@ impl yew::Component for NodePanelComp {
 
     fn create(ctx: &yew::Context<Self>) -> Self {
         ctx.link().send_message(Msg::Reload);
+        ctx.link().send_message(Msg::ReloadStatus);
         Self {
             time_data: Rc::new(Vec::new()),
             cpu_data: Rc::new(Series::new("", Vec::new())),
+            load_data: Rc::new(Series::new("", Vec::new())),
             mem_data: Rc::new(Series::new("", Vec::new())),
             mem_total_data: Rc::new(Series::new("", Vec::new())),
+            status: None,
             last_error: None,
+            last_status_error: None,
             async_pool: AsyncPool::new(),
             _timeout: None,
+            _status_timeout: None,
         }
     }
 
@@ -95,33 +121,61 @@ impl yew::Component for NodePanelComp {
                     Self::reload(&remote, &node).await
                 });
             }
+            Msg::ReloadStatus => {
+                self._status_timeout = None;
+                let props = ctx.props();
+                let remote = props.remote.clone();
+                let node = props.node.clone();
+                self.async_pool.send_future(ctx.link().clone(), async move {
+                    let res = Self::reload_status(&remote, &node).await;
+                    Msg::StatusLoadFinished(res)
+                });
+            }
             Msg::LoadFinished(res) => match res {
                 Ok(data_points) => {
                     self.last_error = None;
                     let mut cpu_vec = Vec::with_capacity(data_points.len());
+                    let mut load_vec = Vec::with_capacity(data_points.len());
                     let mut mem_vec = Vec::with_capacity(data_points.len());
                     let mut mem_total_vec = Vec::with_capacity(data_points.len());
                     let mut time_vec = Vec::with_capacity(data_points.len());
                     for data in data_points {
                         cpu_vec.push(data.cpu_current.unwrap_or(f64::NAN));
+                        load_vec.push(data.cpu_avg1.unwrap_or(f64::NAN));
                         mem_vec.push(data.mem_used.unwrap_or(f64::NAN));
                         mem_total_vec.push(data.mem_total.unwrap_or(f64::NAN));
                         time_vec.push(data.time as i64);
                     }
 
                     self.cpu_data = Rc::new(Series::new(tr!("CPU"), cpu_vec));
+                    self.load_data = Rc::new(Series::new(tr!("Server Load"), load_vec));
                     self.mem_data = Rc::new(Series::new(tr!("Used Memory"), mem_vec));
                     self.mem_total_data = Rc::new(Series::new(tr!("Total Memory"), mem_total_vec));
                     self.time_data = Rc::new(time_vec);
 
                     let link = ctx.link().clone();
                     self._timeout = Some(gloo_timers::callback::Timeout::new(
-                        ctx.props().interval,
+                        ctx.props().rrd_interval,
                         move || link.send_message(Msg::Reload),
                     ))
                 }
                 Err(err) => self.last_error = Some(err),
             },
+
+            Msg::StatusLoadFinished(res) => {
+                match res {
+                    Ok(status) => {
+                        self.last_status_error = None;
+                        self.status = Some(status);
+                    }
+                    Err(err) => self.last_status_error = Some(err),
+                }
+                let link = ctx.link().clone();
+                self._status_timeout = Some(gloo_timers::callback::Timeout::new(
+                    ctx.props().status_interval,
+                    move || link.send_message(Msg::ReloadStatus),
+                ))
+            }
         }
         true
     }
@@ -130,11 +184,16 @@ impl yew::Component for NodePanelComp {
         let props = ctx.props();
 
         if props.remote != old_props.remote || props.node != old_props.node {
-            let remote = props.remote.clone();
-            let node = props.node.clone();
-            self.async_pool.send_future(ctx.link().clone(), async move {
-                Self::reload(&remote, &node).await
-            });
+            self.status = None;
+            self.last_status_error = None;
+            self.last_error = None;
+            self.time_data = Rc::new(Vec::new());
+            self.cpu_data = Rc::new(Series::new("", Vec::new()));
+            self.load_data = Rc::new(Series::new("", Vec::new()));
+            self.mem_data = Rc::new(Series::new("", Vec::new()));
+            self.mem_total_data = Rc::new(Series::new("", Vec::new()));
+            ctx.link()
+                .send_message_batch(vec![Msg::Reload, Msg::ReloadStatus]);
             true
         } else {
             false
@@ -143,11 +202,93 @@ impl yew::Component for NodePanelComp {
 
     fn view(&self, ctx: &yew::Context<Self>) -> yew::Html {
         let props = ctx.props();
+        let title: Html = Row::new()
+            .gap(2)
+            .class(AlignItems::Baseline)
+            .with_child(Fa::new("building"))
+            .with_child(tr! {"node '{0}'", props.node})
+            .into();
+
+        let mut status_comp = Column::new().gap(2).padding(4);
+        if self.status.is_none() && self.last_status_error.is_none() {
+            status_comp.add_child(Progress::new());
+        }
+        if let Some(status) = &self.status {
+            let cpu = status.cpu;
+            let maxcpu = status.cpuinfo.cpus;
+            let load = status.loadavg.join(", ");
+
+            let memory = status.memory.used as u64;
+            let maxmem = status.memory.total as u64;
+            let memory_used = memory as f64 / maxmem as f64;
+
+            let root = status.rootfs.used as u64;
+            let maxroot = status.rootfs.total as u64;
+            let root_used = root as f64 / maxroot as f64;
+
+            status_comp = status_comp
+                .with_child(make_row(
+                    tr!("CPU usage"),
+                    Fa::new("database"),
+                    tr!("{0}% of {1} CPU(s)", format!("{:.2}", cpu * 100.0), maxcpu),
+                    Some(cpu as f32),
+                ))
+                .with_child(make_row(
+                    tr!("Load average"),
+                    Fa::new("line-chart"),
+                    load,
+                    None,
+                ))
+                .with_child(make_row(
+                    tr!("Memory usage"),
+                    Fa::new("list"),
+                    tr!(
+                        "{0}% ({1} of {2})",
+                        format!("{:.2}", memory_used * 100.0),
+                        HumanByte::from(memory),
+                        HumanByte::from(maxmem),
+                    ),
+                    Some(memory_used as f32),
+                ))
+                .with_child(make_row(
+                    tr!("Root filesystem usage"),
+                    Fa::new("server"),
+                    tr!(
+                        "{0}% ({1} of {2})",
+                        format!("{:.2}", root_used * 100.0),
+                        HumanByte::from(root),
+                        HumanByte::from(maxmem),
+                    ),
+                    Some(memory_used as f32),
+                ))
+                .with_child(Container::new().padding(1)) // spacer
+                .with_child(
+                    Row::new()
+                        .with_child(tr!("Version"))
+                        .with_flex_spacer()
+                        .with_child(&status.pveversion),
+                )
+                .with_child(
+                    Row::new()
+                        .with_child(tr!("CPU Model"))
+                        .with_flex_spacer()
+                        .with_child(tr!(
+                            "{0} ({1} sockets)",
+                            status.cpuinfo.model,
+                            status.cpuinfo.sockets
+                        )),
+                );
+        }
+        if let Some(err) = &self.last_status_error {
+            status_comp.add_child(error_message(&err.to_string()));
+        }
+
         Panel::new()
             .class(FlexFit)
-            .title(props.node.as_str())
-            .border(true)
+            .title(title)
             .class(ColorScheme::Neutral)
+            .with_child(status_comp)
+            .with_child(separator().padding_x(4))
             .with_child(
                 Container::new().class(FlexFit).with_child(
                     Column::new()
@@ -167,6 +308,18 @@ impl yew::Component for NodePanelComp {
                         )
                         .with_child(
                             RRDGraph::new(self.time_data.clone())
+                                .title(tr!("Server load"))
+                                .render_value(|v: &f64| {
+                                    if v.is_finite() {
+                                        format!("{:.2}", v)
+                                    } else {
+                                        v.to_string()
+                                    }
+                                })
+                                .serie0(Some(self.load_data.clone())),
+                        )
+                        .with_child(
+                            RRDGraph::new(self.time_data.clone())
                                 .title(tr!("Memory Usage"))
                                 .binary(true)
                                 .render_value(|v: &f64| {
@@ -183,4 +336,8 @@ impl yew::Component for NodePanelComp {
             )
             .into()
     }
+}
+
+fn make_row(title: String, icon: Fa, text: String, meter_value: Option<f32>) -> Column {
+    crate::renderer::status_row(title, icon, text, meter_value, false)
 }
