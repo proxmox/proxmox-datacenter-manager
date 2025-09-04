37dabc0cce5379d9eb8eb9bb0bfdbc518f91175c
Stefan Hanreich <s.hanreich@proxmox.com>
2025-09-04 10:18:48 +0200

    ui: sdn: add EvpnRouteTarget type
    A helper type that can be used to parse the route targets contained in
    the EVPN zones 'Route Target Import' API call.
    
    Signed-off-by: Stefan Hanreich <s.hanreich@proxmox.com>
    Reviewed-by: Dominik Csapak <d.csapak@proxmox.com>
    Tested-by: Dominik Csapak <d.csapak@proxmox.com>

---
+++ ui/src/sdn/evpn/mod.rs
@@ -0,0 +1,26 @@
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Ord, PartialOrd, Hash)]
+pub struct EvpnRouteTarget {
+    asn: u32,
+    vni: u32,
+}
+
+impl std::str::FromStr for EvpnRouteTarget {
+    type Err = anyhow::Error;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        if let Some((asn, vni)) = value.split_once(':') {
+            return Ok(Self {
+                asn: asn.parse()?,
+                vni: vni.parse()?,
+            });
+        }
+
+        anyhow::bail!("could not parse EVPN route target!")
+    }
+}
+
+impl std::fmt::Display for EvpnRouteTarget {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "{}:{}", self.asn, self.vni)
+    }
+}
