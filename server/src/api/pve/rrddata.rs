54f28abb8fa5180dbe81a11bdc032c6f681a8a4e
Dominik Csapak <d.csapak@proxmox.com>
2025-09-08 16:04:14 +0200

    server: api: add rrddata endpoint for pve storages
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- server/src/api/pve/rrddata.rs
+++ server/src/api/pve/rrddata.rs
@@ -8,8 +8,8 @@ use proxmox_rrd_api_types::{RrdMode, RrdTimeframe};
 use proxmox_schema::api;
 
 use pdm_api_types::remotes::REMOTE_ID_SCHEMA;
-use pdm_api_types::rrddata::{LxcDataPoint, NodeDataPoint, QemuDataPoint};
-use pdm_api_types::{NODE_SCHEMA, PRIV_RESOURCE_AUDIT, VMID_SCHEMA};
+use pdm_api_types::rrddata::{LxcDataPoint, NodeDataPoint, PveStorageDataPoint, QemuDataPoint};
+use pdm_api_types::{NODE_SCHEMA, PRIV_RESOURCE_AUDIT, PVE_STORAGE_ID_SCHEMA, VMID_SCHEMA};
 
 use crate::api::rrd_common::{self, DataPoint};
 use crate::metric_collection;
@@ -146,6 +146,27 @@ impl DataPoint for LxcDataPoint {
     }
 }
 
+impl DataPoint for PveStorageDataPoint {
+    fn new(time: u64) -> Self {
+        Self {
+            time,
+            ..Default::default()
+        }
+    }
+
+    fn fields() -> &'static [&'static str] {
+        &["disk_total", "disk_used"]
+    }
+
+    fn set_field(&mut self, name: &str, value: f64) {
+        match name {
+            "disk_total" => self.disk_total = Some(value),
+            "disk_used" => self.disk_used = Some(value),
+            _ => {}
+        }
+    }
+}
+
 #[api(
     input: {
         properties: {
@@ -233,6 +254,37 @@ async fn get_node_rrd_data(
     get_rrd_datapoints(remote, base, timeframe, cf).await
 }
 
+#[api(
+    input: {
+        properties: {
+            remote: { schema: REMOTE_ID_SCHEMA },
+            node: { schema: NODE_SCHEMA },
+            storage: { schema: PVE_STORAGE_ID_SCHEMA },
+            timeframe: {
+                type: RrdTimeframe,
+            },
+            cf: {
+                type: RrdMode,
+            },
+        },
+    },
+    access: {
+        permission: &Permission::Privilege(&["resource", "{remote}", "storage", "{storage}"], PRIV_RESOURCE_AUDIT, false),
+    },
+)]
+/// Read node stats
+async fn get_storage_rrd_data(
+    remote: String,
+    node: String,
+    storage: String,
+    timeframe: RrdTimeframe,
+    cf: RrdMode,
+    _param: Value,
+) -> Result<Vec<NodeDataPoint>, Error> {
+    let base = format!("pve/{remote}/storage/{node}/{storage}");
+    get_rrd_datapoints(remote, base, timeframe, cf).await
+}
+
 async fn get_rrd_datapoints<T: DataPoint + Send + 'static>(
     remote: String,
     basepath: String,
@@ -260,3 +312,4 @@ async fn get_rrd_datapoints<T: DataPoint + Send + 'static>(
 pub const QEMU_RRD_ROUTER: Router = Router::new().get(&API_METHOD_GET_QEMU_RRD_DATA);
 pub const LXC_RRD_ROUTER: Router = Router::new().get(&API_METHOD_GET_LXC_RRD_DATA);
 pub const NODE_RRD_ROUTER: Router = Router::new().get(&API_METHOD_GET_NODE_RRD_DATA);
+pub const STORAGE_RRD_ROUTER: Router = Router::new().get(&API_METHOD_GET_STORAGE_RRD_DATA);
