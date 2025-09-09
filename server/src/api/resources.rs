56a80782168ce5cc7f5da6eaafe533d9f791ef38
Dominik Csapak <d.csapak@proxmox.com>
2025-09-09 10:25:51 +0200

    server: api: resources: use root cause for errors for remote fetching
    when we can't reach a remote for any reason, we want to return the error
    as a string here over the API. Since most errors that can occur here are
    client/network related (wrong credentials, no route to host, timeout,
    etc.) converting this error directly to a string gives us
    errors like:
    
    `client error (Connect)`
    
    which is not really helpful most of the time.
    Instead if we use the `root_cause()`, we get the most underlying error
    
    e.g.
    
    `error connecting to https://0.0.0.0:8006/ - tcp connect error: No route to host (os error 113)`
    
    which is much more helpful.
    
    We could also think about printing the whole error chain, but in my test
    cases here this was not more helpful, e.g. i got two times the above
    `client error (Connect)` and once the root cause from above.
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
    Link: https://lore.proxmox.com/20250909082608.1018103-1-d.csapak@proxmox.com

--- server/src/api/resources.rs
+++ server/src/api/resources.rs
@@ -250,7 +250,7 @@ pub(crate) async fn get_resources_impl(
         let handle = tokio::spawn(async move {
             let (mut resources, error) = match get_resources_for_remote(remote, max_age).await {
                 Ok(resources) => (resources, None),
-                Err(error) => (Vec::new(), Some(error.to_string())),
+                Err(error) => (Vec::new(), Some(error.root_cause().to_string())),
             };
 
             if remotes_only {
