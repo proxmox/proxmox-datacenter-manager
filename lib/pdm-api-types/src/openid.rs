e7c4bbe582f7eb4838574315fcd4737f89646fd8
Shannon Sterz <s.sterz@proxmox.com>
2025-10-17 15:58:01 +0200

    api-types: add default field to openid realm config
    Signed-off-by: Shannon Sterz <s.sterz@proxmox.com>
    Link: https://lore.proxmox.com/20251017135802.363955-8-s.sterz@proxmox.com

--- lib/pdm-api-types/src/openid.rs
+++ lib/pdm-api-types/src/openid.rs
@@ -109,6 +109,9 @@ pub struct OpenIdRealmConfig {
     pub client_key: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub comment: Option<String>,
+    /// True if you want this to be the default realm selected on login.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub default: Option<bool>,
     /// Automatically create users if they do not exist.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub autocreate: Option<bool>,
