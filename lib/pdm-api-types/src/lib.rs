918f726a68a015921f3dfbb8327f97ef5eb11a9c
Lukas Wagner <l.wagner@proxmox.com>
2025-11-11 11:50:52 +0100

    pdm-api-types: move RemoteUpid to its own module
    lib.rs re-exports all public member of the new remote_upid module, so
    for callers this change is non-breaking.
    
    Signed-off-by: Lukas Wagner <l.wagner@proxmox.com>
    Reviewed-by: Shannon Sterz <s.sterz@proxmox.com>
    Tested-by: Shannon Sterz <s.sterz@proxmox.com>
    Link: https://lore.proxmox.com/20251111105059.148997-2-l.wagner@proxmox.com

--- lib/pdm-api-types/src/lib.rs
+++ lib/pdm-api-types/src/lib.rs
@@ -1,13 +1,11 @@
 //! Basic API types used by most of the PDM code.
 
 use std::collections::HashMap;
-use std::fmt;
 
-use anyhow::{bail, Error};
 use const_format::concatcp;
 use serde::{Deserialize, Serialize};
 
-use proxmox_schema::api_types::{DNS_NAME_STR, IPRE_BRACKET_STR, PORT_REGEX_STR, SAFE_ID_REGEX};
+use proxmox_schema::api_types::{DNS_NAME_STR, IPRE_BRACKET_STR, PORT_REGEX_STR};
 use proxmox_schema::{
     api, const_regex, ApiStringFormat, ApiType, ArraySchema, IntegerSchema, ReturnType, Schema,
     StringSchema, Updater,
@@ -22,6 +20,9 @@ pub use node_config::*;
 
 mod metric_collection;
 pub use metric_collection::*;
+
+mod remote_upid;
+pub use remote_upid::*;
 
 mod proxy;
 pub use proxy::HTTP_PROXY_SCHEMA;
@@ -409,90 +410,6 @@ impl ConfigurationState {
 
 serde_plain::derive_display_from_serialize!(ConfigurationState);
 serde_plain::derive_fromstr_from_deserialize!(ConfigurationState);
-
-pub const REMOTE_UPID_SCHEMA: Schema = StringSchema::new("A remote UPID")
-    .min_length("C!UPID:N:12345678:12345678:12345678:::".len())
-    .schema();
-
-#[derive(Clone, Debug, Eq, PartialEq, Hash)]
-pub struct RemoteUpid {
-    remote: String,
-    /// This is usually a pve upid, but may also be a pbs upid, they have distinct formats.
-    pub upid: String,
-}
-
-impl RemoteUpid {
-    pub fn remote(&self) -> &str {
-        &self.remote
-    }
-
-    pub fn into_remote(self) -> String {
-        self.remote
-    }
-}
-
-impl ApiType for RemoteUpid {
-    const API_SCHEMA: Schema = REMOTE_UPID_SCHEMA;
-}
-
-impl TryFrom<(String, String)> for RemoteUpid {
-    type Error = Error;
-
-    fn try_from((remote, upid): (String, String)) -> Result<Self, Error> {
-        if !SAFE_ID_REGEX.is_match(&remote) {
-            bail!("bad remote id in remote upid");
-        }
-        Ok(Self { remote, upid })
-    }
-}
-
-impl TryFrom<(String, &str)> for RemoteUpid {
-    type Error = Error;
-
-    fn try_from((remote, upid): (String, &str)) -> Result<Self, Error> {
-        if !SAFE_ID_REGEX.is_match(&remote) {
-            bail!("bad remote id in remote upid");
-        }
-        Ok(Self {
-            remote,
-            upid: upid.to_string(),
-        })
-    }
-}
-
-impl TryFrom<(&str, &str)> for RemoteUpid {
-    type Error = Error;
-
-    fn try_from((remote, upid): (&str, &str)) -> Result<Self, Error> {
-        if !SAFE_ID_REGEX.is_match(remote) {
-            bail!("bad remote id in remote upid");
-        }
-        Ok(Self {
-            remote: remote.to_string(),
-            upid: upid.to_string(),
-        })
-    }
-}
-
-impl std::str::FromStr for RemoteUpid {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self, Error> {
-        match s.find('!') {
-            None => bail!("missing '!' separator in remote upid"),
-            Some(pos) => (&s[..pos], &s[(pos + 1)..]).try_into(),
-        }
-    }
-}
-
-impl fmt::Display for RemoteUpid {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}!{}", self.remote, self.upid)
-    }
-}
-
-serde_plain::derive_deserialize_from_fromstr!(RemoteUpid, "valid remote upid");
-serde_plain::derive_serialize_from_display!(RemoteUpid);
 
 fn limit_default() -> u64 {
     50
---
+++ lib/pdm-api-types/src/remote_upid.rs
@@ -0,0 +1,93 @@
+use std::fmt;
+
+use anyhow::{bail, Error};
+
+use proxmox_schema::api_types::SAFE_ID_REGEX;
+use proxmox_schema::{ApiType, Schema, StringSchema};
+
+pub const REMOTE_UPID_SCHEMA: Schema = StringSchema::new("A remote UPID")
+    .min_length("C!UPID:N:12345678:12345678:12345678:::".len())
+    .schema();
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub struct RemoteUpid {
+    remote: String,
+    /// This is usually a pve upid, but may also be a pbs upid, they have distinct formats.
+    pub upid: String,
+}
+
+impl RemoteUpid {
+    pub fn remote(&self) -> &str {
+        &self.remote
+    }
+
+    pub fn into_remote(self) -> String {
+        self.remote
+    }
+}
+
+impl ApiType for RemoteUpid {
+    const API_SCHEMA: Schema = REMOTE_UPID_SCHEMA;
+}
+
+impl TryFrom<(String, String)> for RemoteUpid {
+    type Error = Error;
+
+    fn try_from((remote, upid): (String, String)) -> Result<Self, Error> {
+        if !SAFE_ID_REGEX.is_match(&remote) {
+            bail!("bad remote id in remote upid");
+        }
+
+        Ok(Self { remote, upid })
+    }
+}
+
+impl TryFrom<(String, &str)> for RemoteUpid {
+    type Error = Error;
+
+    fn try_from((remote, upid): (String, &str)) -> Result<Self, Error> {
+        if !SAFE_ID_REGEX.is_match(&remote) {
+            bail!("bad remote id in remote upid");
+        }
+
+        Ok(Self {
+            remote,
+            upid: upid.to_string(),
+        })
+    }
+}
+
+impl TryFrom<(&str, &str)> for RemoteUpid {
+    type Error = Error;
+
+    fn try_from((remote, upid): (&str, &str)) -> Result<Self, Error> {
+        if !SAFE_ID_REGEX.is_match(remote) {
+            bail!("bad remote id in remote upid");
+        }
+
+        Ok(Self {
+            remote: remote.to_string(),
+            upid: upid.to_string(),
+        })
+    }
+}
+
+impl std::str::FromStr for RemoteUpid {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Error> {
+        match s.find('!') {
+            None => bail!("missing '!' separator in remote upid"),
+            Some(pos) => (&s[..pos], &s[(pos + 1)..]).try_into(),
+        }
+    }
+}
+
+impl fmt::Display for RemoteUpid {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}!{}", self.remote, self.upid)
+    }
+}
+
+serde_plain::derive_deserialize_from_fromstr!(RemoteUpid, "valid remote upid");
+serde_plain::derive_serialize_from_display!(RemoteUpid);
