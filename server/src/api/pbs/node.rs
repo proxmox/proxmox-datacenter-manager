e27c29b9e35bfef9e4b03ebf3d3d8a1525a6bd76
Fabian Grünbichler <f.gruenbichler@proxmox.com>
2025-11-11 09:29:31 +0100

    api: pbs: wire up node shell endpoints
    Tested-by: Stefan Hanreich <s.hanreich@proxmox.com>
    Reviewed-by: Stefan Hanreich <s.hanreich@proxmox.com>
    Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

--- server/src/api/pbs/node.rs
+++ server/src/api/pbs/node.rs
@@ -6,4 +6,14 @@ pub const ROUTER: Router = Router::new()
     .subdirs(SUBDIRS);
 
 #[sortable]
-const SUBDIRS: SubdirMap = &sorted!([("apt", &crate::api::remote_updates::APT_ROUTER),]);
+const SUBDIRS: SubdirMap = &sorted!([
+    ("apt", &crate::api::remote_updates::APT_ROUTER),
+    (
+        "termproxy",
+        &Router::new().post(&crate::api::remote_shell::API_METHOD_SHELL_TICKET)
+    ),
+    (
+        "vncwebsocket",
+        &Router::new().upgrade(&crate::api::remote_shell::API_METHOD_SHELL_WEBSOCKET)
+    ),
+]);
