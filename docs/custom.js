b03d684baef8b720df588a79873e944ddd746543
Thomas Lamprecht <t.lamprecht@proxmox.com>
2025-12-02 08:15:54 +0100

    docs: drop duplicate and unused custom.{css,js} files
    We use the ones in the _static/ directory, these got just committed by
    accident.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- docs/custom.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    let activeSection = document.querySelector("a.current");
-    if (activeSection) {
-        // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
-        activeSection.scrollIntoView({ block: 'center' });
-    }
-});
