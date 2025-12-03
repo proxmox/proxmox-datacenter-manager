proxmox/proxmox-datacenter-manager

commit 5502c3c308b6e448d0ba8ebfefd3ac7ef9236546

api-types/api: add endpoints for querying the node's status
Signed-off-by: Shannon Sterz <s.sterz@proxmox.com>
Reviewed-by: Dominik Csapak <d.csapak@proxmox.com>
Tested-by: Dominik Csapak <d.csapak@proxmox.com>
Link: https://lore.proxmox.com/20251106124334.189955-5-s.sterz@proxmox.com

server/src/bin/proxmox-datacenter-api/main.rs
@@ -391,6 +391,8 @@ fn make_tls_acceptor() -> Result<SslAcceptor, Error> {
     let key_path = configdir!("/auth/api.key");
     let cert_path = configdir!("/auth/api.pem");
 
+    proxmox_node_status::init_node_status_api(cert_path)?;
+
     proxmox_rest_server::connection::TlsAcceptorBuilder::new()
         .certificate_paths_pem(key_path, cert_path)
         .build()

Cargo.toml
@@ -78,6 +78,7 @@ proxmox-time-api = "1"
 proxmox-network-api = "1"
 proxmox-syslog-api = "1"
 proxmox-acme-api = "1"
+proxmox-node-status = "1"
 
 # API types for PVE (and later PMG?)
 pve-api-types = "8.0.5"
@@ -163,6 +164,7 @@ zstd = { version = "0.13" }
 # proxmox-log = { path = "../proxmox/proxmox-log" }
 # proxmox-metrics = { path = "../proxmox/proxmox-metrics" }
 # proxmox-network-api = { path = "../proxmox/proxmox-network-api" }
+# proxmox-node-status = { path = "../proxmox/proxmox-node-status" }
 # proxmox-notify = { path = "../proxmox/proxmox-notify" }
 # proxmox-openid = { path = "../proxmox/proxmox-openid" }
 # proxmox-product-config = { path = "../proxmox/proxmox-product-config" }

lib/pdm-api-types/src/acl.rs
@@ -26,6 +26,8 @@ constnamedbitmap! {
         PRIV_SYS_MODIFY("System.Modify");
         /// `Sys.Console` allows access to the system's console
         PRIV_SYS_CONSOLE("Sys.Console");
+        /// `Sys.PowerManagement` allows powering off or rebooting the system.
+        PRIV_SYS_POWER_MANAGEMENT("Sys.PowerManagement");
 
         /// `Resource.Audit` allows auditing guests, storages and other resources.
         PRIV_RESOURCE_AUDIT("Resource.Audit");

server/Cargo.toml
@@ -72,6 +72,7 @@ proxmox-time-api = { workspace = true, features = [ "impl" ] }
 proxmox-network-api = { workspace = true, features = [ "impl" ] }
 proxmox-syslog-api = { workspace = true, features = [ "impl" ] }
 proxmox-acme-api = { workspace = true, features = [ "impl" ] }
+proxmox-node-status = { workspace = true, features = [ "api" ] }
 
 pdm-api-types.workspace = true
 pdm-buildcfg.workspace = true

server/src/api/nodes/mod.rs
@@ -10,6 +10,7 @@ pub mod dns;
 pub mod journal;
 pub mod network;
 pub mod rrddata;
+pub mod status;
 pub mod syslog;
 pub mod tasks;
 pub mod termproxy;
@@ -45,6 +46,7 @@ pub const SUBDIRS: SubdirMap = &sorted!([
     ("journal", &journal::ROUTER),
     ("network", &network::ROUTER),
     ("rrdata", &rrddata::ROUTER),
+    ("status", &status::ROUTER),
     ("syslog", &syslog::ROUTER),
     ("tasks", &tasks::ROUTER),
     ("termproxy", &termproxy::ROUTER),

server/src/api/nodes/status.rs
@@ -1,0 +1,18 @@
+use pdm_api_types::{PRIV_SYS_AUDIT, PRIV_SYS_POWER_MANAGEMENT};
+use proxmox_router::{ApiMethod, Permission, Router};
+
+const API_METHOD_GET_STATUS_WITH_ACCESS: ApiMethod = proxmox_node_status::API_METHOD_GET_STATUS
+    .access(
+        None,
+        &Permission::Privilege(&["system", "status"], PRIV_SYS_AUDIT, false),
+    );
+
+const API_METHOD_REBOOT_OR_SHUTDOWN_WITH_ACCESS: ApiMethod =
+    proxmox_node_status::API_METHOD_REBOOT_OR_SHUTDOWN.access(
+        None,
+        &Permission::Privilege(&["system", "status"], PRIV_SYS_POWER_MANAGEMENT, false),
+    );
+
+pub const ROUTER: Router = Router::new()
+    .get(&API_METHOD_GET_STATUS_WITH_ACCESS)
+    .post(&API_METHOD_REBOOT_OR_SHUTDOWN_WITH_ACCESS);

commit f40ce5f6e690fcbac2a16ada45febe6a1df8c303

api daemon: do not init node status API inside tls acceptor
Not only does this make zero semantic sense to do so there, but it
actually breaks the re-creating the acceptor in the reload-certificate
command, as that would then try to re-init a singleton, which results
in an error.

Might be slightly nicer to add a common const for the cert paths, but
they are unlikely to change, and if one those without a grep for the
old name for safety then there is not much we can hedge against...

Fixes: 5502c3c ("api-types/api: add endpoints for querying the node's status")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

server/src/bin/proxmox-datacenter-api/main.rs
@@ -194,6 +194,8 @@ async fn run(debug: bool) -> Result<(), Error> {
     let redirector = proxmox_rest_server::Redirector::new();
     proxmox_rest_server::init_worker_tasks(pdm_buildcfg::PDM_LOG_DIR_M!().into(), file_opts)?;
 
+    proxmox_node_status::init_node_status_api(configdir!("/auth/api.pem"))?;
+
     //openssl req -x509 -newkey rsa:4096 -keyout /etc/proxmox-backup/api.key -out /etc/proxmox-backup/api.pem -nodes
 
     // we build the initial acceptor here as we cannot start if this fails
@@ -390,8 +392,6 @@ async fn run(debug: bool) -> Result<(), Error> {
 fn make_tls_acceptor() -> Result<SslAcceptor, Error> {
     let key_path = configdir!("/auth/api.key");
     let cert_path = configdir!("/auth/api.pem");
-
-    proxmox_node_status::init_node_status_api(cert_path)?;
 
     proxmox_rest_server::connection::TlsAcceptorBuilder::new()
         .certificate_paths_pem(key_path, cert_path)