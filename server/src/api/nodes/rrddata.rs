10807815534845fe51ad0ea9aa9a58226f7f231f
Lukas Wagner <l.wagner@proxmox.com>
2026-04-24 13:34:16 +0200

    pdm-api-types: add PDM host metric fields
    Signed-off-by: Lukas Wagner <l.wagner@proxmox.com>
    Reviewed-by: Arthur Bied-Charreton <a.bied-charreton@proxmox.com>
    Reviewed-by: Michael Köppl <m.koeppl@proxmox.com>
    Tested-by: Arthur Bied-Charreton <a.bied-charreton@proxmox.com>
    Tested-by: Michael Köppl <m.koeppl@proxmox.com>
    Link: https://lore.proxmox.com/20260424113418.278367-5-l.wagner@proxmox.com

--- server/src/api/nodes/rrddata.rs
+++ server/src/api/nodes/rrddata.rs
@@ -18,12 +18,61 @@ impl DataPoint for PdmNodeDatapoint {
     }
 
     fn fields() -> &'static [&'static str] {
-        &["metric-collection-total-time"]
+        &[
+            "cpu-current",
+            "cpu-iowait",
+            "cpu-avg1",
+            "cpu-avg5",
+            "cpu-avg15",
+            "cpu-pressure-some-avg10",
+            "disk-total",
+            "disk-used",
+            "disk-read-iops",
+            "disk-write-iops",
+            "disk-read",
+            "disk-write",
+            "disk-io-ticks",
+            "io-pressure-some-avg10",
+            "io-pressure-full-avg10",
+            "mem-total",
+            "mem-used",
+            "mem-pressure-some-avg10",
+            "mem-pressure-full-avg10",
+            "swap-total",
+            "swap-used",
+            "net-in",
+            "net-out",
+            "metric-collection-total-time",
+        ]
     }
 
     fn set_field(&mut self, name: &str, value: f64) {
-        if name == "metric-collection-total-time" {
-            self.metric_collection_total_time = Some(value);
+        match name {
+            "cpu-current" => self.cpu_current = Some(value),
+            "cpu-iowait" => self.cpu_iowait = Some(value),
+            "cpu-avg1" => self.cpu_avg1 = Some(value),
+            "cpu-avg5" => self.cpu_avg5 = Some(value),
+            "cpu-avg15" => self.cpu_avg15 = Some(value),
+            "cpu-pressure-some-avg10" => self.cpu_pressure_some_avg10 = Some(value),
+            "disk-total" => self.disk_total = Some(value),
+            "disk-used" => self.disk_used = Some(value),
+            "disk-read-iops" => self.disk_read_iops = Some(value),
+            "disk-write-iops" => self.disk_write_iops = Some(value),
+            "disk-read" => self.disk_read = Some(value),
+            "disk-write" => self.disk_write = Some(value),
+            "disk-io-ticks" => self.disk_io_ticks = Some(value),
+            "io-pressure-some-avg10" => self.io_pressure_some_avg10 = Some(value),
+            "io-pressure-full-avg10" => self.io_pressure_full_avg10 = Some(value),
+            "mem-total" => self.mem_total = Some(value),
+            "mem-used" => self.mem_used = Some(value),
+            "mem-pressure-some-avg10" => self.mem_pressure_some_avg10 = Some(value),
+            "mem-pressure-full-avg10" => self.mem_pressure_full_avg10 = Some(value),
+            "swap-total" => self.swap_total = Some(value),
+            "swap-used" => self.swap_used = Some(value),
+            "net-in" => self.net_in = Some(value),
+            "net-out" => self.net_out = Some(value),
+            "metric-collection-total-time" => self.metric_collection_total_time = Some(value),
+            _ => log::error!("setting invalid field '{name}' in PdmNodeDatapoint"),
         }
     }
 }
