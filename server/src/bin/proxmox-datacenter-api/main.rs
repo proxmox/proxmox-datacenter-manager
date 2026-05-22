39b36565cee582f2aace228603c9128b0f8e7ba9
Dominik Csapak <d.csapak@proxmox.com>
2026-05-22 10:34:05 +0200

    server: serve geojson worldmap
    this will be used by the ui to show a world map with remote information
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
    Tested-by: Shannon Sterz <s.sterz@proxmox.com>
    Reviewed-by: Shannon Sterz <s.sterz@proxmox.com>
    Link: https://lore.proxmox.com/20260522083412.1223719-10-d.csapak@proxmox.com
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- server/src/bin/proxmox-datacenter-api/main.rs
+++ server/src/bin/proxmox-datacenter-api/main.rs
@@ -168,6 +168,7 @@ async fn run(debug: bool) -> Result<(), Error> {
             ("xtermjs", "/usr/share/pve-xtermjs"),
             ("locale", "/usr/share/pdm-i18n"),
             ("docs", "/usr/share/doc/proxmox-datacenter-manager/html"),
+            ("geojson", "/usr/share/proxmox-geojson-data"),
         ])
         .formatted_router(&["api2"], &server::api::ROUTER)
         // FIXME: disabled for testing on pure debian
