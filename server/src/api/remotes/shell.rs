cc7796c0f861c287e255da94f4558b4325c65452
Wolfgang Bumiller <w.bumiller@proxmox.com>
2026-05-29 14:48:30 +0200

    server: refactor websocket upgrade
    Move `upgrade_to_websocket` into an `async fn` to drop 2 levels of
    indentations and prepare for some later refactoring.
    
    Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

--- server/src/api/remotes/shell.rs
+++ server/src/api/remotes/shell.rs
@@ -1,5 +1,5 @@
 use anyhow::{Context, Error, bail, format_err};
-use futures::{FutureExt, TryFutureExt};
+use futures::TryFutureExt;
 use http::{
     Method, Request, StatusCode,
     header::{SEC_WEBSOCKET_KEY, SEC_WEBSOCKET_VERSION, UPGRADE},
@@ -128,134 +128,138 @@ fn upgrade_to_websocket(
     _info: &ApiMethod,
     rpcenv: Box<dyn RpcEnvironment>,
 ) -> ApiResponseFuture {
-    async move {
-        // intentionally user only for now
-        let auth_id: Authid = rpcenv
-            .get_auth_id()
-            .context("no authid available")?
-            .parse()?;
+    Box::pin(upgrade_to_websocket_do(parts, req_body, param, rpcenv))
+}
 
-        if auth_id.is_token() {
-            bail!("API tokens cannot access this API endpoint");
+async fn upgrade_to_websocket_do(
+    parts: http::request::Parts,
+    req_body: hyper::body::Incoming,
+    param: serde_json::Value,
+    rpcenv: Box<dyn RpcEnvironment>,
+) -> Result<http::Response<proxmox_http::Body>, Error> {
+    // intentionally user only for now
+    let auth_id: Authid = rpcenv
+        .get_auth_id()
+        .context("no authid available")?
+        .parse()?;
+
+    if auth_id.is_token() {
+        bail!("API tokens cannot access this API endpoint");
+    }
+
+    let userid = auth_id.user();
+    let ticket = required_string_param(&param, "vncticket")?;
+
+    let public_auth_keyring = Keyring::with_public_key(crate::auth::key::public_auth_key().clone());
+
+    let remote = required_string_param(&param, "remote")?.to_owned();
+    let node = required_string_param(&param, "node")?.to_owned();
+    let ticket_path = encode_term_ticket_path(&remote, &node);
+
+    Ticket::<Empty>::parse(ticket)?.verify(
+        &public_auth_keyring,
+        crate::auth::TERM_PREFIX,
+        Some(&format!("{}{}", userid, ticket_path)),
+    )?;
+
+    let (mut ws, response) = WebSocket::new(parts.headers.clone())?;
+
+    proxmox_rest_server::spawn_internal_task(async move {
+        let incoming_ws: Upgraded = match hyper::upgrade::on(Request::from_parts(parts, req_body))
+            .map_err(Error::from)
+            .await
+        {
+            Ok(upgraded) => upgraded,
+            _ => bail!("error"),
+        };
+
+        let (remotes, _digest) = pdm_config::remotes::config()?;
+        let remote = get_remote(&remotes, &remote)?;
+        let (ticket, port) = match remote.ty {
+            RemoteType::Pve => {
+                let pve = crate::connection::make_pve_client(remote)?;
+                let pve_term_ticket = pve
+                    .node_shell_termproxy(
+                        &node,
+                        pve_api_types::NodeShellTermproxy {
+                            cmd: None,
+                            cmd_opts: None,
+                        },
+                    )
+                    .await?;
+                (pve_term_ticket.ticket, pve_term_ticket.port)
+            }
+            RemoteType::Pbs => {
+                let pbs = crate::connection::make_pbs_client(remote)?;
+                let pbs_term_ticket = pbs.node_shell_termproxy().await?;
+                (pbs_term_ticket.ticket, pbs_term_ticket.port as i64)
+            }
+        };
+
+        let raw_client = crate::connection::make_raw_client(remote)?;
+
+        let ws_key = proxmox_sys::linux::random_data(16)?;
+        let ws_key = proxmox_base64::encode(&ws_key);
+
+        let api_url = raw_client.api_url().clone().into_parts();
+
+        let mut builder = http::uri::Builder::new();
+        if let Some(scheme) = api_url.scheme {
+            builder = builder.scheme(scheme);
+        }
+        if let Some(authority) = api_url.authority {
+            builder = builder.authority(authority)
+        }
+        let api_path = ApiPathBuilder::new(format!("/api2/json/nodes/{node}/vncwebsocket"))
+            .arg("vncticket", ticket.clone())
+            .arg("port", port)
+            .build();
+        let uri = builder
+            .path_and_query(api_path)
+            .build()
+            .map_err(|err| format_err!("failed to build Uri - {err}"))?;
+
+        let auth = raw_client.login_auth()?;
+        let req = Request::builder()
+            .method(Method::GET)
+            .uri(uri)
+            .header(UPGRADE, "websocket")
+            .header(SEC_WEBSOCKET_VERSION, "13")
+            .header(SEC_WEBSOCKET_KEY, ws_key);
+
+        let req = auth.set_auth_headers(req).body(Body::empty())?;
+
+        let res = raw_client.http_client().request(req).await?;
+        if res.status() != StatusCode::SWITCHING_PROTOCOLS {
+            bail!("server didn't upgrade: {}", res.status());
         }
 
-        let userid = auth_id.user();
-        let ticket = required_string_param(&param, "vncticket")?;
+        let pve_ws = hyper::upgrade::on(res)
+            .await
+            .map_err(|err| format_err!("failed to upgrade - {}", err))?;
 
-        let public_auth_keyring =
-            Keyring::with_public_key(crate::auth::key::public_auth_key().clone());
+        let username = if let proxmox_client::AuthenticationKind::Token(ref token) = *auth {
+            token.userid.clone()
+        } else {
+            bail!("shell not supported with ticket-based authentication")
+        };
 
-        let remote = required_string_param(&param, "remote")?.to_owned();
-        let node = required_string_param(&param, "node")?.to_owned();
-        let ticket_path = encode_term_ticket_path(&remote, &node);
+        let preamble = format!("{username}:{ticket}\n", ticket = ticket);
+        ws.mask = Some([0, 0, 0, 0]);
 
-        Ticket::<Empty>::parse(ticket)?.verify(
-            &public_auth_keyring,
-            crate::auth::TERM_PREFIX,
-            Some(&format!("{}{}", userid, ticket_path)),
-        )?;
+        if let Err(err) = ws
+            .proxy_connection(
+                TokioIo::new(incoming_ws),
+                TokioIo::new(pve_ws),
+                preamble.as_bytes(),
+            )
+            .await
+        {
+            log::warn!("error while copying between websockets: {err:?}");
+        }
 
-        let (mut ws, response) = WebSocket::new(parts.headers.clone())?;
+        Ok(())
+    });
 
-        proxmox_rest_server::spawn_internal_task(async move {
-            let incoming_ws: Upgraded =
-                match hyper::upgrade::on(Request::from_parts(parts, req_body))
-                    .map_err(Error::from)
-                    .await
-                {
-                    Ok(upgraded) => upgraded,
-                    _ => bail!("error"),
-                };
-
-            let (remotes, _digest) = pdm_config::remotes::config()?;
-            let remote = get_remote(&remotes, &remote)?;
-            let (ticket, port) = match remote.ty {
-                RemoteType::Pve => {
-                    let pve = crate::connection::make_pve_client(remote)?;
-                    let pve_term_ticket = pve
-                        .node_shell_termproxy(
-                            &node,
-                            pve_api_types::NodeShellTermproxy {
-                                cmd: None,
-                                cmd_opts: None,
-                            },
-                        )
-                        .await?;
-                    (pve_term_ticket.ticket, pve_term_ticket.port)
-                }
-                RemoteType::Pbs => {
-                    let pbs = crate::connection::make_pbs_client(remote)?;
-                    let pbs_term_ticket = pbs.node_shell_termproxy().await?;
-                    (pbs_term_ticket.ticket, pbs_term_ticket.port as i64)
-                }
-            };
-
-            let raw_client = crate::connection::make_raw_client(remote)?;
-
-            let ws_key = proxmox_sys::linux::random_data(16)?;
-            let ws_key = proxmox_base64::encode(&ws_key);
-
-            let api_url = raw_client.api_url().clone().into_parts();
-
-            let mut builder = http::uri::Builder::new();
-            if let Some(scheme) = api_url.scheme {
-                builder = builder.scheme(scheme);
-            }
-            if let Some(authority) = api_url.authority {
-                builder = builder.authority(authority)
-            }
-            let api_path = ApiPathBuilder::new(format!("/api2/json/nodes/{node}/vncwebsocket"))
-                .arg("vncticket", ticket.clone())
-                .arg("port", port)
-                .build();
-            let uri = builder
-                .path_and_query(api_path)
-                .build()
-                .map_err(|err| format_err!("failed to build Uri - {err}"))?;
-
-            let auth = raw_client.login_auth()?;
-            let req = Request::builder()
-                .method(Method::GET)
-                .uri(uri)
-                .header(UPGRADE, "websocket")
-                .header(SEC_WEBSOCKET_VERSION, "13")
-                .header(SEC_WEBSOCKET_KEY, ws_key);
-
-            let req = auth.set_auth_headers(req).body(Body::empty())?;
-
-            let res = raw_client.http_client().request(req).await?;
-            if res.status() != StatusCode::SWITCHING_PROTOCOLS {
-                bail!("server didn't upgrade: {}", res.status());
-            }
-
-            let pve_ws = hyper::upgrade::on(res)
-                .await
-                .map_err(|err| format_err!("failed to upgrade - {}", err))?;
-
-            let username = if let proxmox_client::AuthenticationKind::Token(ref token) = *auth {
-                token.userid.clone()
-            } else {
-                bail!("shell not supported with ticket-based authentication")
-            };
-
-            let preamble = format!("{username}:{ticket}\n", ticket = ticket);
-            ws.mask = Some([0, 0, 0, 0]);
-
-            if let Err(err) = ws
-                .proxy_connection(
-                    TokioIo::new(incoming_ws),
-                    TokioIo::new(pve_ws),
-                    preamble.as_bytes(),
-                )
-                .await
-            {
-                log::warn!("error while copying between websockets: {err:?}");
-            }
-
-            Ok(())
-        });
-
-        Ok(response)
-    }
-    .boxed()
+    Ok(response)
 }
